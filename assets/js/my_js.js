fb7d941efee2812b8aed9da613dc221d30692141
lorenagayoso <lorena.gayoso.g@gmail.com>
2021-10-26 18:44:16 -0300

    first commit

---
+++ assets/js/my_js.js
@@ -0,0 +1,387 @@
+const toppings = document.querySelectorAll('input[name="topping-radio"]');
+
+const size = $("select[name='size']")[0];
+
+const delivery = $("select[name='delivery']")[0];
+
+const qty = $("input#quantity")[0];
+
+const submit = $("#submit")[0];
+
+const precio_caba = $("#caba_price")[0];
+
+const message_caba = $("#caba_message")[0];
+
+const delivery_message = $("#delivery_message")[0];
+
+const clock_message = $("#clock_message")[0];
+
+const clock_message2 = $("#clock_message2")[0];
+
+const clock_element = $("#clock")[0];
+
+
+/*document.addEventListener("DomContentLoaded", jQuery.getJSON('https://ipinfo.io/json', function myData(data) {
+const location = document.getElementById("location");
+location.innerText = JSON.parse(JSON.stringify(data,null,2))["city"];
+;}))*/
+
+$(document).ready(function() {$.getJSON('https://ipinfo.io/json', function myData(data) {
+const location = $("#location")[0];
+location.innerText = JSON.parse(JSON.stringify(data,null,2))["city"];
+;})})
+
+
+if((new Date().getHours() < 18)){
+                              promo_index = 0
+                              delivery.options[4].innerText = "CABA (GRATIS!)"
+                              delivery.options[4].value = 4
+                              ;} 
+
+    else {
+         promo_index = 1
+         delivery.options[4].innerText = "CABA ($150)"
+         delivery.options[4].value = 3
+         ;}
+
+const 
+
+precios_size =
+{
+  media_docena : 300 ,
+  docena : 550 ,
+  promo_20_18 : 700 
+}
+
+precios_delivery =
+{
+  norte : 250 ,
+  sur : 500 ,
+  oeste : 700 ,
+  caba : 150 ,
+  retira : 0
+}
+
+
+const
+
+promo_time = {
+  color : ["color : black","color : lightgrey"] ,
+  texto: ["Pedí GRATIS ahora en CABA :)", "Ups! No es horario de promo :("] ,
+  precio : [0, 150] ,
+  mensaje : "Envío gratis antes de las 18" ,
+  mensaje_estilo: [{
+                  "color": "#e83e8c",
+                  "font-size": "inherit",
+                  "font-weight": "bold" ,
+                  "display": "flex",
+                  "align-items": "center", 
+                  "flex-direction": "column",
+                  },
+                  {
+                  "margin" :"auto",
+                  "width" :"80%",
+                  "color":"#8261ee",
+                  "font-size": "larger",
+                  "font-weight": "bold" ,
+                  "display": "flex",
+                  "align-items": "center",
+                  "text-align": "center", 
+                  "flex-direction": "column"
+                  }],
+
+  mensaje_estilo_2: [{
+                  "display": "none",
+                  },
+                  {
+                  "display": "flex",
+                  "align-items": "center",
+                  "text-align": "center", 
+                  "flex-direction": "column",
+                  "margin" :"auto",
+                  "width" :"80%",
+                  "color":"black",
+                  "margin-top": "5px",
+                  "font-size": "regular",
+                  }]
+
+}
+
+function tour_start() {
+introJs().start();
+}
+
+function clock() {
+let time = new Date(),
+    
+    
+    hours = time.getHours(),
+    
+    
+    minutes = time.getMinutes(),
+    
+    
+    seconds = time.getSeconds();
+
+  $('.clock')[0].innerHTML = tiempo(hours) + ":" + tiempo(minutes) + ":" + tiempo(seconds);
+  
+  function tiempo(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
+}
+
+setInterval(clock, 1000);
+
+clock_element.setAttribute("style", promo_time.color[promo_index])
+
+clock_message.innerText = promo_time.texto[promo_index]
+
+precio_caba.innerText = promo_time.precio[promo_index]
+
+message_caba.innerText = promo_time.mensaje
+
+Object.assign(clock_message.style, promo_time.mensaje_estilo[promo_index])
+
+Object.assign(clock_message2.style, promo_time.mensaje_estilo_2[promo_index])
+
+
+$("input[value = 'none']")[0].checked = true
+
+
+toppings.forEach((i) => {
+    i.addEventListener("change", function() {
+      let relleno_selected = $("select[name='relleno']")[0].options[relleno.selectedIndex].value;
+      let topping_selected = i.value;
+
+      if(topping_selected == relleno_selected) 
+      {
+
+        Swal.fire({
+        showDenyButton: true,
+        icon: 'question' ,
+        title: '<strong>Seleccionaste el mismo gusto para relleno y topping</strong>',
+        text : '¿Esto es lo que quieres?',
+        confirmButtonText: 'Lo sé, AMO',
+        denyButtonText: 'Me equivoqué'
+        }).then((result) => {
+        if (result.isConfirmed) {
+         Swal.fire('Perfecto!' , 'También amamos nuestro gusto de ' + relleno_selected)
+        } else if (result.isDenied) {
+        Swal.fire('De acuerdo, no los seleccionaremos :)')
+        $("input[value = 'none']")[0].checked = true
+        $("option[value='none']")[0].selected = true;
+        }
+        })
+
+	  ;}
+
+    else if((topping_selected == "Blueberry") || (topping_selected == "Limon")) 
+    {
+
+    $("input[value = 'Blueberry']")[0].checked = false;
+    $("input[value = 'Limon']")[0].checked = false;
+
+    let response =  prompt("No contamos con este gusto :( .Estos son nuestros gustos disponibles: Chocolate, Frutilla o Maracuyá");
+
+    function success_gusto(){Swal.fire('Excelente!',
+                               'Este sabor también te encantará. Lo seleccionaremos por ti :)',
+                               'success')}
+
+    if(response == null)       {
+                                Swal.fire("Si no seleccionas un nuevo topping, no podrás generar un pedido :(")
+                                $("input[value = 'none']")[0].checked = true
+                               ;}
+    
+		else if(response.length > 0) {
+                                if((response.normalize("NFD").replace(/[\u0300-\u036f]/g, "")).toLowerCase() == "maracuya") 
+                                {
+                                success_gusto()
+                                $("input[value = 'Maracuya']")[0].checked = true
+                                ;}
+
+
+                                else if((response.normalize("NFD").replace(/[\u0300-\u036f]/g, "")).toLowerCase() == "frutilla") 
+                                {
+                                success_gusto()
+                                $("input[value = 'Frutilla']")[0].checked = true
+                                ;}
+
+                                else if((response.normalize("NFD").replace(/[\u0300-\u036f]/g, "")).toLowerCase() == "chocolate") 
+                                {
+                                success_gusto()
+                                $("input[value = 'Chocolate']")[0].checked = true
+                                ;}
+
+                            ;}
+
+
+      else if(response.length == 0) {
+                                Swal.fire("Selecciona otro topping y vuelve a intentarlo")
+                                $("input[value = 'none']")[0].checked = true
+                               ;}
+
+
+	}})});
+
+ 
+function toppings_selection(){
+ for(i=0 ; i < toppings.length ; i++) {
+  if(toppings[i].checked == true) {return toppings[i].value}
+ ;}
+ ;}
+
+function order_selection(){
+
+let relleno_selected = $("select[name='relleno']")[0].options[relleno.selectedIndex].value;
+
+let topping_selected = toppings_selection();
+
+let size_chosen = size.options[size.selectedIndex];
+
+let delivery_chosen = delivery.options[delivery.selectedIndex] ;
+
+let size_fee = Object.values(precios_size)[size_chosen.value] ;
+
+let qty_value = qty.value ;
+
+let delivery_fee = Object.values(precios_delivery)[delivery_chosen.value] ;
+
+let precio_current = (size_fee * qty_value) + delivery_fee ;
+
+let orden_resumen = 
+[
+"<strong>Relleno:</strong>" + " " + relleno_selected ,
+"<strong>Topping:</strong> " + " " + toppings_selection(),
+"<strong>Presentacion:</strong> " + " " + size_chosen.innerText.split(" (")[0] ,
+"<strong>Cantidad:</strong> " + " " + qty_value ,
+"<strong>Envio:</strong> " + " " + delivery_chosen.innerText,
+"<strong>Total + IVA(21%):</strong>" + " " + precio_current * 1.21
+];
+
+return orden_resumen
+;}
+
+
+
+function order_format(ordenParam) {
+for (let i = 0; i < ordenParam.length; i++) {
+ordenParam[i] = "<br>" + ordenParam[i] + "<br>";
+}
+return ordenParam.join(" ");
+}
+
+
+
+//FUNCIONES DE LOS BOTONES
+
+
+//CALCULO DEL ENVIO
+
+function envio_calc(){
+
+let size_chosen = size.options[size.selectedIndex];
+
+let delivery_chosen = delivery.options[delivery.selectedIndex];
+
+if((size_chosen.value == "empty") || (delivery_chosen.value == "empty")) {
+
+Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Olvidaste info importante para calcular tu envío',
+  footer: 'Completá los campos 3 y 4'
+})
+
+}
+
+else {
+
+let subtotal_resumen = order_selection().slice(-4);
+
+order_format(subtotal_resumen)
+
+Swal.fire({
+  title: "Envío estimado",
+  html : order_format(subtotal_resumen),
+  width: 400,
+  padding: '3em',
+})
+
+;}
+
+;}
+
+
+// SOLICITUD DE ORDEN 
+
+function submit_order(){
+
+let relleno_selected = $("select[name='relleno']")[0].options[relleno.selectedIndex];
+
+let topping_selected = toppings_selection();
+
+let size_chosen = size.options[size.selectedIndex];
+
+let delivery_chosen = delivery.options[delivery.selectedIndex];
+
+if((size_chosen.value == "empty") || (delivery_chosen.value == "empty") || (relleno_selected.value == "none") || (toppings_selection() == "none")) {
+
+Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Olvidaste info importante para generar tu orden',
+  footer: 'Asegúrate de completar todos los campos'
+})
+
+;}
+
+else {
+
+let total_resumen = order_selection();
+
+Swal.fire({
+  showDenyButton: true,
+  title: "Resumen de la orden",
+  html : order_format(total_resumen),
+  width: 400,
+  padding: '3em',
+  confirmButtonText: 'Si, lo quiero!',
+  denyButtonText: 'Hmm... no estoy segur@'
+}).then((result) => {
+  if (result.isConfirmed) {
+  Swal.fire({
+  title: 'Hemos recibido tu orden!',
+  text: 'Confirmanos tu nombre para continuar con tu pedido',
+  input: 'text',
+  inputAttributes: {
+    autocapitalize: 'off'
+  },
+  confirmButtonText: 'Ahi va'
+  }).then((result) => {
+  if (result.isConfirmed) {
+  localStorage.setItem("name" , result.value )
+  Swal.fire(
+  'Gracias,' + result.value,
+  'En unos minutos, nos comunicamos con vos',
+  'success'
+  )
+  }})
+  }})
+}
+}
+
+
+// BORRAR SELECCION
+
+function reset_values(){
+$("#result").innerText = "" ;
+;}
+
+
+/* 	
+    let topping_tooltip_text = document.querySelector(".box input:checked ~ .tooltiptext").textContent
+
+*/
